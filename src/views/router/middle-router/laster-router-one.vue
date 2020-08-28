5d9bc214a816ccdca8af3e1e6b8d754e773acda3
luoschenfeng <luoschenfeng@gmail.com>
2020-08-29 00:54:32 +0800

    组件

--- src/views/router/middle-router/laster-router-one.vue
+++ src/views/router/middle-router/laster-router-one.vue
@@ -10,6 +10,12 @@
     <el-checkbox v-model="checked">
       备选项
     </el-checkbox>
+    <el-button
+      type="text"
+      @click="open"
+    >
+      点击打开 Message Box
+    </el-button>
   </div>
 </template>
 
@@ -20,7 +26,19 @@ export default {
       checked: true,
     }
   },
-  methods: {},
+  methods: {
+    open() {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${ action }`,
+          });
+        },
+      });
+    },
+  },
 }
 </script>
 
--- src/views/router/middle-router/laster-router-two.vue
+++ src/views/router/middle-router/laster-router-two.vue
@@ -4,11 +4,62 @@
     <router-link :to="{name: 'Dashboard'}">
       Dashboard
     </router-link>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      />
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+      />
+      <el-table-column
+        prop="address"
+        label="地址"
+      />
+    </el-table>
+    <el-pagination
+      layout="prev, pager, next"
+      :total="1000"
+      style="text-align: center"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
